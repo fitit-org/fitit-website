7df4173f643e797620a241d3368e85215192f8fc
MikolajJaworowski <jaworowskimikolaj@gmail.com>
2021-01-29 02:29:36 +0100

    Initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './index.css';
 
 function App() {
   return (
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="text-red-500 App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
